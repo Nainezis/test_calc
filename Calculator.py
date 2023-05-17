0d3c90a88c51790df0268c01ae3dd85c92844ee2
Timur Kushakov <timurk98@mail.ru>
2023-05-18 00:41:36 +0300

    calculator

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,23 @@
 
+from collections import deque
+ 
+dexp = deque(list(input()))
+ 
+def mop(val1, op, val2):
+    if op == '+':
+        return int(val1) + int(val2)
+    if op == '-':
+        return int(val1) - int(val2)
+    if op == '*':
+        return int(val1) * int(val2)
+    if op == '/':
+        return int(val1) / int(val2)
+ 
+def calc(dexp):
+    if len(dexp) == 1:
+        return dexp[0]
+    dexp.appendleft(mop(dexp.popleft(), dexp.popleft(), dexp.popleft()))
+    return calc(dexp)
+ 
+ 
+print(calc(dexp))
